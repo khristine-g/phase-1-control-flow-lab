cd9255125a408c8c23a33baace6bc17f3c5fa95c
khristine-g <githigekhristine@gmail.com>
2023-03-15 21:33:11 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
-function scuberGreetingForFeet(distance){
-  
-  
-    if (distance<= 400) {
-      return 'This one is on me!';
-    } else if (distance > 2000 && distance<= 2500) {
-      return 'I will gladly take your thirty bucks.';
-    } else if (distance > 2500) {
-      return 'No can do.';
-    }
+function scuberGreetingForFeet(distance) {
+  let greeting;
+
+  if (distance <= 400) {
+    greeting = 'This one is on me!';
+  } else if (distance> 2000 && distance<= 2500) {
+    greeting = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    greeting = 'No can do.';
   }
-  
+
+  return greeting;
+}
 
 
-function ternaryCheckCity(){
-    return (destination === 'NYC') ? 'Ok, sounds good.' : 'No go.';
-  }
-  
-
-
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
 
 function switchOnCharmFromTip(tip) {
-    switch (tip) {
-      case 'generous':
-        return 'Thank you so much.';
-      case 'not as generous':
-        return 'Thank you.';
-      default:
-        return 'Bye.';
-    }
+  let response;
+
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
   }
 
- 
+  return response;
+}
